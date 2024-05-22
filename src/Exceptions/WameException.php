6f2fc584b1735ba4f0bcc13801feb9f2d48e8e75
WAME s.r.o <info@wame.sk>
2024-05-22 15:37:54 +0200

    Update WameException

--- src/Exceptions/WameException.php
+++ src/Exceptions/WameException.php
@@ -3,12 +3,5 @@
 namespace Wame\Core\Exceptions;
 
 use Exception;
-use Illuminate\Contracts\Routing\ResponseFactory;
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Contracts\Foundation\Application as ContractsApplication;
 
-class WameException extends Exception
-{
-}
+class WameException extends Exception { }
